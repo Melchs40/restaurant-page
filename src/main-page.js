8712c866ff32f30c76cbc3636512e59936d06b71
Melchs40 <samscodingjourney@gmail.com>
2023-10-25 12:00:47 -0500

    Edit content for main page in main-page.js and add in Sass/CSS for page design

--- src/main-page.js
+++ src/main-page.js
@@ -1,6 +1,16 @@
+import Picture from "./bar-image.jpeg";
+
 export default function createPage() {
+
     const header = document.createElement("h1");
     header.innerHTML = "Welcome to The Lizard Lounge";
     document.getElementById("content").appendChild(header);
-    console.log("test success");
+
+    const picture = new Image();
+    picture.src = Picture;
+    document.getElementById("content").appendChild(picture);
+
+    const mainText = document.createElement("div");
+    mainText.innerHTML = "The Lizard Lounge is Oshkosh's premier craft beer and microbrew bar. We have a variety of beers from Wisconsin, the United States, and the world. Come check us out; you won't be disappointed."
+    document.getElementById("content").appendChild(mainText);
 }
